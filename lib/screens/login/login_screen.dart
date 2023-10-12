1fcbec8856978f495995a4031aa5b45fc6077ff0
zyad negm <zyadosamanegm@gmail.com>
2023-10-12 17:30:48 +0300

    loginUI

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,11 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_app/screens/Home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 class Login_Screen extends StatelessWidget {
   static const String routeName = "login";
 
+
   @override
   Widget build(BuildContext context) {
+    Future<UserCredential> signInWithGoogle() async {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    }
+
     return Scaffold(
       body: Column(
         children: [
@@ -53,7 +74,21 @@ class Login_Screen extends StatelessWidget {
           SizedBox(height: 20,),
           Text("or Login with",style: TextStyle(fontSize: 22,fontWeight: FontWeight.w400,color: Colors.white),),
           SizedBox(height: 13,),
-          ClipOval(child: Image.asset("assets/images/download.png",width: 100,height: 100,),),
+          Row(children: [
+            InkWell(onTap: () {
+              signInWithGoogle();
+
+            },
+                child: ClipOval(child: Image.asset("assets/images/Google.png",width: 100,height: 100,),)),
+
+            InkWell(onTap: () {
+
+            },
+                child: ClipOval(child: Image.asset("assets/images/Facebook.jpeg",width: 100,height: 100,),)),
+
+
+          ]
+            ,)
 
           
 
